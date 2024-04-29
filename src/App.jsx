81d2ca835b711befef10f9dfd46d8fe469a4d7d9
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2024-04-29 13:03:16 +0300

    todo app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [addTodo, setAddTodo] = useState({todoName: "", isTodoDone: false});
+
+  const addNewTodo = () => {
+    const removeSpace = addTodo.todoName.trim().replaceAll(/\s+/g, ' ');
+
+    if(removeSpace !== ""){
+      setTodos(t => [...t, addTodo]);
+      setAddTodo({todoName: "", isTodoDone: false});
+    }
+  }
+
+  const toggleTodo = (index) => {
+    setTodos(todos.map((todo, i) => {
+      if (i === index) {
+        return { ...todo, isTodoDone: !todo.isTodoDone };
+      }
+      return todo;
+    }));
+  }
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  }
+
+  const moveTodoUp = (index) => {
+    if(index > 0){
+      const updatedTasks = [...todos];
+      [updatedTasks[index], updatedTasks[index - 1]] = 
+      [updatedTasks[index - 1], updatedTasks[index]];
+      setTodos(updatedTasks);
+    }
+  }
+
+  const moveTodoDown = (index) => {
+    if(index < todos.length - 1){
+      const updatedTasks = [...todos];
+      [updatedTasks[index], updatedTasks[index + 1]] = 
+      [updatedTasks[index + 1], updatedTasks[index]];
+      setTodos(updatedTasks);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>To-Do-List</h1>
+      
+      <input type="text" name="todoName" value={addTodo.todoName} onChange={(event) => setAddTodo({...todos, [event.target.name]: event.target.value})} placeholder="Write To-Do"/>
+      <button onClick={addNewTodo}>Add</button>
+
+      <table>
+        {todos.map((todo, index) => 
+        <tr key={index}>
+            <td>{todo.isTodoDone === true ? <del>{todo.todoName}</del> : todo.todoName}</td>
+            <td onClick={() => toggleTodo(index)}>{todo.isTodoDone === true ? "Undone" : "Done"}</td>
+            <td onClick={() => deleteTodo(index)}>❌</td>
+            <td onClick={() => moveTodoUp(index)}>👆</td>
+            <td onClick={() => moveTodoDown(index)}>👇</td>
+          </tr>)}
+      </table>
+    </div>
   )
 }
 
